7bc663a413c09f2f9776da26cdb916c549ae86f5
badrallehyani <badr.a.allehyani@gmail.com>
2025-02-25 15:15:17 +0300

    clean & format

--- src/components/smaller_components/Workshop.jsx
+++ src/components/smaller_components/Workshop.jsx
@@ -1,32 +1,32 @@
 import React from 'react';
 
 function Workshop({ title, description, time, date, iconURL, i }) {
-    return (
-        <div class="swiper-slide slide-custom">
-            <div className={"upper upper" + (i + 1)}></div>
-            <img class="workshop-icon" src={iconURL} />
-            <div class="text-container">
-                <h3>{title}</h3>
-                <p>
-                    {description}
-                </p>
-            </div>
-            <div class="time-details">
-                <div class="time-item">
-                    <img class="xx" src="/images/mingcute_time-line.svg" />
-                    <p class="time-text">
-                        {time}
-                    </p>
-                </div>
-                <div class="time-item">
-                    <img class="xx" src="/images/typcn_calendar-outline.svg" />
-                    <p class="time-text">
-                        {date}
-                    </p>
-                </div>
-            </div>
-        </div>
-    )
+	return (
+		<div class="swiper-slide slide-custom">
+			<div className={"upper upper" + (i + 1)}></div>
+			<img class="workshop-icon" src={iconURL} />
+			<div class="text-container">
+				<h3>{title}</h3>
+				<p>
+					{description}
+				</p>
+			</div>
+			<div class="time-details">
+				<div class="time-item">
+					<img class="xx" src="/images/mingcute_time-line.svg" />
+					<p class="time-text">
+						{time}
+					</p>
+				</div>
+				<div class="time-item">
+					<img class="xx" src="/images/typcn_calendar-outline.svg" />
+					<p class="time-text">
+						{date}
+					</p>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Workshop;
